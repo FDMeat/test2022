abec696272f6f7fcc598fda12c219ea0ef489a0e
FDMeat <vlad.zubrilin@yandex.ru>
2022-02-10 18:09:07 +0400

    first test is working

--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -1,18 +1,42 @@
 package tests;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pages.SearchPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTests {
 
-    @Test
-    public void test1() {
+    private static WebDriver driver;
+    private static SearchPage searchPage;
+
+
+    @BeforeAll
+    public static void init() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://google.com");
-        driver.quit();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        searchPage= new SearchPage(driver);
+    }
+        @Test
+        public void test1() {
+            driver.get("http://google.com");
+            searchPage.search("selenium");
+            assertEquals(12, searchPage.results.size());
     }
 
 
+        @AfterAll
+    public static void teardown(){
+        driver.quit();
+
+    }
 }
